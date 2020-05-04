a10880993dab10964a899427daf55262a828e52f
carlos2140485 <carlossibim210@gmail.com>
2020-05-04 20:06:26 -0300

    VARIÁVEIS

--- Unidade 2/capitulo 1.py
+++ Unidade 2/capitulo 1.py
@@ -2,3 +2,20 @@ print('Hello word')
 print(2)
 a = 2
 type(a)
+
+mensagem = 'Oi Python'
+print(mensagem)
+print()
+numero = 5
+pi = 3.14
+type(mensagem)
+
+print(1)
+x = 2
+print(x)
+
+ab = 1 + 1
+print(ab)
+
+ac = 2 * 3
+print(ac)
